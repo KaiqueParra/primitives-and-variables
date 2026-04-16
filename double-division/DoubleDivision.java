aeb9ce8a40c7880712969c326756aef25c17cbc9
KaiqueParra <kaique.p.parra@gmail.com>
2026-04-16 19:20:26 -0300

    feat: add double division example

---
+++ double-division/DoubleDivision.java
@@ -0,0 +1,13 @@
+public class DoubleDivision {
+    
+    public static void main(String[] args) {
+        
+        System.out.println("Total payable R$ 35.58");
+
+        double total = 35.58, numberOfPeople = 2.0, result;
+
+        result = total / numberOfPeople;
+
+        System.out.println("Total payable a two people: " + result );
+    }
+}
