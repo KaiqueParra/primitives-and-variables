ac8b3bafb92bcd1a0416d8fda60e298cf2ecdbc8
KaiqueParra <kaique.p.parra@gmail.com>
2026-04-16 19:14:46 -0300

    feat: add int sum example

---
+++ int-sum/IntSum.java
@@ -0,0 +1,11 @@
+public class IntSum{
+
+    public static void main(String[] args){
+
+        int number1 = 5, number2 = 7, result;
+
+        result = number1 + number2;
+
+        System.out.println(number1 + " + " + number2 + " = " + result);
+    }
+}
