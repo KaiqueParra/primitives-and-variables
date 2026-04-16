9382470639ba1766855b06c19dc6353ee288a291
KaiqueParra <kaique.p.parra@gmail.com>
2026-04-16 20:25:06 -0300

    feat: add short game points example

---
+++ short-game-points/ShortGamePoints.java
@@ -0,0 +1,11 @@
+public class ShortGamePoints {
+    
+    public static void main(String[] args) {
+    
+        short round1 = 50, round2 = 32, total;
+
+        total = (short) (round1 + round2);
+
+        System.out.println("Total game is: " + total);
+    }
+}
