cc0ce0316a3e2fc7c665f2bf804c710553e8c634
KaiqueParra <kaique.p.parra@gmail.com>
2026-04-16 20:05:27 -0300

    feat: add float average example

---
+++ float-average/FloatAverage.java
@@ -0,0 +1,11 @@
+public class FloatAverage {
+    
+    public static void main(String[] args) {
+        
+        float score1 = 3.2F , score2 = 9.5F, result;
+
+        result = (score1 + score2) / 2;
+
+        System.out.printf("The average is: %.2f", result);
+    }
+}
