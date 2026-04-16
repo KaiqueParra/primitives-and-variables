7a5cad448fb21d35a4632b72ea528349cdc4d518
KaiqueParra <kaique.p.parra@gmail.com>
2026-04-16 20:30:29 -0300

    feat: add int multiplication example

---
+++ int-multiplication/IntMultiplication.java
@@ -0,0 +1,10 @@
+public class IntMultiplication {
+    
+    public static void main(String[] args) {
+        
+        int value = 37, quantity = 7, result; 
+
+        result = value * quantity;
+        System.out.println("Total to pay: " + result);
+    }
+}
